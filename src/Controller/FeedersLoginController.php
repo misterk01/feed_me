2342a1ad284aa65a5556bebba0563e7d5379aed2
Damien Baudet <damienbaudet@free.fr>
2019-10-23 17:22:20 +0200

    création des controllers et views FeedersLogin

---
+++ src/Controller/FeedersLoginController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class FeedersLoginController extends AbstractController
+{
+
+    /**
+     * Display FeedersLogin page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('FeedersLogin/index.html.twig');
+    }
+}
